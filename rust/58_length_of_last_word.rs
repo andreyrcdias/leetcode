2db1165f161e722930cbed92ebbac68638947cce
Andrey <andreyrcdias@gmail.com>
2025-01-26 08:15:04 -0300

    rust: 26 remove duplicates from sorted arr

---
+++ rust/58_length_of_last_word.rs
@@ -0,0 +1,18 @@
+fn length_of_last_word(s: String) -> i32 {
+    let words: Vec<_> = s.split_whitespace().collect();
+    let last_word = words.last().unwrap();
+    last_word.len() as i32
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_length_of_last_word() {
+        assert_eq!(length_of_last_word(String::from("Hello World")), 5);
+        assert_eq!(length_of_last_word(String::from("   fly me   to   the moon  ")), 4);
+        assert_eq!(length_of_last_word(String::from("luffy is still joyboy")), 6);
+    }
+}
+
