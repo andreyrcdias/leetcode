f5eff22bbf8de59bf001e4699a5f5d844915290c
Andrey <andreyrcdias@gmail.com>
2025-01-28 08:29:05 -0300

    python: 2884 modify columns

---
+++ python/2884_modify_columns.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["salary"] = employees["salary"] * 2
+    return employess
+
+
+data = {
+    'name': ['Jack', 'Piper', 'Mia', 'Ulysses'],
+    'salary': [19666, 74754, 62509, 54866],
+}
+employess = pd.DataFrame(data)
+modifySalaryColumn(employess)
+print(employess)
