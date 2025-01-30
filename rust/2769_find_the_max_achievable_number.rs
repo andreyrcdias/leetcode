c2a06d9a217d56bae5142024b2c08a4552cc9097
Andrey <andreyrcdias@gmail.com>
2025-01-30 08:05:12 -0300

    rust: 2769 find the max achievable number

---
+++ rust/2769_find_the_max_achievable_number.rs
@@ -0,0 +1,14 @@
+fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+    num + t * 2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_the_maximum_achievable_x() {
+        assert_eq!(the_maximum_achievable_x(4, 1), 6);
+        assert_eq!(the_maximum_achievable_x(3, 2), 7);
+    }
+}
