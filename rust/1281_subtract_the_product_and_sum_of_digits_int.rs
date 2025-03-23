8facfee71b292063ca6b01bc4bb8116ff96af23e
Andrey <andreyrcdias@gmail.com>
2025-03-23 07:17:58 -0300

    rust: 1281 rm pritnln

--- rust/1281_subtract_the_product_and_sum_of_digits_int.rs
+++ rust/1281_subtract_the_product_and_sum_of_digits_int.rs
@@ -1,6 +1,5 @@
 fn subtract_product_and_sum(n: i32) -> i32 {
     let ns = n.to_string();
-    println!("{}", ns);
     let nsv: Vec<i32> = ns
         .chars()
         .filter_map(|c| c.to_digit(10))
