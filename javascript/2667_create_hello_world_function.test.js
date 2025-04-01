a156d61f6404147b6e62648527062121f70c519c
Andrey <andreyrcdias@gmail.com>
2025-04-01 15:12:45 -0300

    js: running prettier

--- javascript/2667_create_hello_world_function.test.js
+++ javascript/2667_create_hello_world_function.test.js
@@ -1,22 +1,21 @@
-var createHelloWorld = function(...args) {
-	return function(...args) {
-		return "Hello World";
-	}
-}
+var createHelloWorld = function (...args) {
+  return function (...args) {
+    return "Hello World";
+  };
+};
 
-describe('createHelloWorld', () => {
-	test('case 1: empty arguments', () => {
-		const args = [];
-		const got = createHelloWorld(...args)();
-		const expected = "Hello World";
-		expect(got).toBe(expected);
-	});
+describe("createHelloWorld", () => {
+  test("case 1: empty arguments", () => {
+    const args = [];
+    const got = createHelloWorld(...args)();
+    const expected = "Hello World";
+    expect(got).toBe(expected);
+  });
 
-	test('case 2: multiple arguments', () => {
-		const args = [{}, null, 42];
-		const got = createHelloWorld(...args)();
-		const expected = "Hello World";
-		expect(got).toBe(expected);
-	});
+  test("case 2: multiple arguments", () => {
+    const args = [{}, null, 42];
+    const got = createHelloWorld(...args)();
+    const expected = "Hello World";
+    expect(got).toBe(expected);
+  });
 });
-
