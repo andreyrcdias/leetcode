95d88960b59c1a5bbda656536b59b56161615111
Andrey <andreyrcdias@gmail.com>
2025-05-04 20:43:38 -0300

    rust: 1351 count negative numbers in a sorted matrix

---
+++ rust/1351_count_negative_numbers_in_sorted_matrix.rs
@@ -0,0 +1,31 @@
+fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
+    let mut negatives = 0;
+    for row in grid {
+        for num in row {
+            if num < 0 {
+                println!("{}", num);
+                negatives += 1;
+            }
+        }
+    }
+    negatives
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_count_negatives() {
+        assert_eq!(
+            count_negatives(vec![
+                vec![4, 3, 2, -1],
+                vec![3, 2, 1, -1],
+                vec![1, 1, -1, -2],
+                vec![-1, -1, -2, -3],
+            ]),
+            8
+        );
+        assert_eq!(count_negatives(vec![vec![3, 2], vec![1, 0]]), 0);
+    }
+}
