8c11f6657328eb29af325625c91bcd29060271d4
Andrey <andreyrcdias@gmail.com>
2025-05-08 08:30:31 -0300

    rust: added run shell script

--- rust/run.sh
+++ rust/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <program_name>"
+    echo "Usage: $0 <program_name.rs>"
     exit 1
 fi
 
 PROGRAM_NAME=$1
-rustc "${PROGRAM_NAME}.rs" --test && ./"${PROGRAM_NAME}"
+rustc "${PROGRAM_NAME}" --test && ./"${PROGRAM_NAME}"
 
