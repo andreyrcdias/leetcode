da4ef06d41688e4d8eee42b7996dca4a52174cba
Andrey <andreyrcdias@gmail.com>
2025-06-03 16:07:00 -0300

    rust: 350 intersection of two arrays 2

--- rust/350_intersection_of_two_arrays_2.rs
+++ rust/350_intersection_of_two_arrays_2.rs
@@ -1,21 +1,17 @@
 use std::collections::HashMap;
 
 fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-    if nums1.len() > nums2.len() {
-        return intersect(nums2.clone(), nums2);
-    }
-
-    let mut count = HashMap::new();
+    let mut counter = HashMap::new();
     for &num in &nums1 {
-        *count.entry(num).or_insert(0) += 1;
+        *counter.entry(num).or_insert(0) += 1;
     }
 
     let mut ans = Vec::new();
     for &num in &nums2 {
-        if let Some(&c) = count.get(&num) {
-            if c > 0 {
+        if let Some(count) = counter.get_mut(&num) {
+            if *count > 0 {
                 ans.push(num);
-                count.insert(num, c - 1);
+                *count -= 1;
             }
         }
     }
