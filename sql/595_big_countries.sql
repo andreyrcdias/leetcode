6a85ff4bce439fd5a35ea812b021752c73464779
Andrey <andreyrcdias@gmail.com>
2025-07-06 17:25:47 -0300

    sql: 595 big countries

---
+++ sql/595_big_countries.sql
@@ -0,0 +1,15 @@
+-- World
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | name        | varchar |
+-- | continent   | varchar |
+-- | area        | int     |
+-- | population  | int     |
+-- | gdp         | bigint  |
+-- +-------------+---------+
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000
+
