12b5317b7d27ec9e2c18093b4ef985436e54b3a0
Andrey <andreyrcdias@gmail.com>
2025-07-20 11:56:40 -0300

    sql: 175 combine two tables

---
+++ sql/175_combine_two_tables.sql
@@ -0,0 +1,27 @@
+-- Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+
+-- Address
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+
+SELECT
+	p.FirstName,
+	p.LastName,
+	a.City,
+	a.State
+FROM Person p
+LEFT JOIN Address a ON p.PersonId = a.PersonId
+
