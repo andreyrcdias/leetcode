f8f156a99a7911ad2976d7de418ded61802f773b
Andrey <andreyrcdias@gmail.com>
2025-07-23 08:26:30 -0300

    sql: 176 second highest salary

---
+++ sql/176_second_highest_salary.sql
@@ -0,0 +1,12 @@
+-- Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+
+SELECT email AS Email
+FROM Person
+GROUP BY Email
+HAVING COUNT(*) > 1;
